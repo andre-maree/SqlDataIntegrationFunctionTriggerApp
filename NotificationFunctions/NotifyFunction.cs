f4fa32ba995a6746cafb81ecab570159402a254e
Maree, Andre D. (LNG-CON) <mareea@legal.regn.net>
2026-01-27 20:02:42 +0200

    remove comment

--- NotificationFunctions/NotifyFunction.cs
+++ NotificationFunctions/NotifyFunction.cs
@@ -2,7 +2,6 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.DurableTask;
 using Microsoft.DurableTask.Entities;
 using Microsoft.Extensions.Logging;
-//using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SqlDataIntegrationFunctionTriggerApp;
 
